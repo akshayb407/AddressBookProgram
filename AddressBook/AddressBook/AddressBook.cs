331a00806756768ea7c4efcd310de5be1d750bd9
akshayb407 <akshayb407@gmail.com>
2022-06-21 18:39:55 +0530

    master added

--- AddressBook/AddressBook/AddressBook.cs
+++ AddressBook/AddressBook/AddressBook.cs
@@ -6,58 +6,36 @@ using System.Threading.Tasks;
 
 namespace AddressBook
 {
-    internal class AddressBook
+    class UC1_CreateContact
     {
-        public String firstName;
-        public String lastName;
-        public String address;
-        public String state;
-        public string city;
-        public long zip;
-        public long phoneNumber;
-        public String email;
-
-     
-        public static void DisplayMessage()
+        // method for creating single contact and displaying contact
+        public void CreateContact()
         {
-            Console.WriteLine("Welcome to Address Book AddressBook");
+            string contact;
+            Console.WriteLine("Please Enter Contact Details\n");
+            Console.Write("First Name : ");
+            String firstName = Console.ReadLine();
+            Console.Write("Last Name : ");
+            String lastName = Console.ReadLine();
+            Console.Write("Phone Number : ");
+            long phoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.Write("Email : ");
+            String email = Console.ReadLine(); ;
+            Console.Write("Address : ");
+            String address = Console.ReadLine();
+            Console.Write("City : ");
+            String city = Console.ReadLine();
+            Console.Write("State : ");
+            String state = Console.ReadLine();
+            Console.Write("Zip : ");
+            int zipCode = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("\nContact Created.\n");
+
+            contact = firstName + " " + lastName + " " + phoneNumber.ToString() + " " + email + " " + address + " " + city + " " + state + " " + zipCode.ToString();
+
+            Console.WriteLine(contact);
         }
-
-        public static void SetContact()
-        {
-            AddressBook addressBook = new AddressBook();
-            addressBook.firstName = "Akshay";
-            addressBook.lastName = "Bhagwat";
-            addressBook.address = "Pune Katrj";
-            addressBook.state = "Maharashtra";
-            addressBook.city = "Pune";
-            addressBook.zip = 411046;
-            addressBook.phoneNumber = 8087148746;
-            addressBook.email = "akshayb407@gmail.com";
-
-        }
-        public static void SetContact(String firstName, String lastName, String address, String city, String state, long zip, long phNo)
-        {
-            AddressBook addressBook = new AddressBook();
-            addressBook.firstName = firstName;
-            Console.WriteLine("Enter firstName");
-            addressBook.firstName = Console.ReadLine();
-            addressBook.lastName = lastName;
-            Console.WriteLine("Enter lastName");
-            addressBook.lastName =Console.ReadLine();
-            addressBook.address = address;
-            addressBook.city = city;
-            Console.WriteLine("Enter City");
-            addressBook.address=Console.ReadLine();
-            addressBook.state = state;
-            addressBook.zip = zip;
-            addressBook.phoneNumber = phNo;
-           
-
-        }
-        
-       
-
-      
     }
 }
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -6,8 +6,28 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            
-         AddressBook.DisplayMessage();
+
+
+
+            Console.WriteLine("Welcome to Address Book\n");
+            UC1_CreateContact uc1 = new UC1_CreateContact();
+            uc1.CreateContact();
+
+
+            Console.WriteLine("UC2 starting");
+            UC2AddNewContacts uc2 = new UC2AddNewContacts();
+            uc2.AddNewContacts();
+
+            Console.WriteLine("UC3 starting");
+            UC3UpdateExistingContact uc3 = new UC3UpdateExistingContact();
+            uc3.UpdateContact();
+
+            Console.WriteLine("UC4 starting");
+            UC4DeleteContact uc4 = new UC4DeleteContact();
+            uc4.ContactDelete();
+
+            Console.ReadKey();
+
 
         }
     }

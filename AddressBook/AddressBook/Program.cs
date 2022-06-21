6972a7123a7de620c71e6b4d99849266e5668e65
akshayb407 <akshayb407@gmail.com>
2022-06-21 19:15:08 +0530

    UC2 added

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -6,8 +6,17 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            
-         AddressBook.DisplayMessage();
+
+
+            Console.WriteLine("Welcome to Address Book\n");
+            UC1CreateContact uc1 = new UC1CreateContact();
+            uc1.CreateContact();
+
+
+            Console.WriteLine("UC2 starting");
+            UC2AddNewContacts uc2 = new UC2AddNewContacts();
+            uc2.AddNewContacts();
+
 
         }
     }

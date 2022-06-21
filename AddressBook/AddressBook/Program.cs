e94fc1dcd43e9ec353e96d6c277623f4a48be5e8
akshayb407 <akshayb407@gmail.com>
2022-06-21 18:54:15 +0530

    UC1 added

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -6,8 +6,14 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            
-         AddressBook.DisplayMessage();
+
+
+
+            Console.WriteLine("Welcome to Address Book\n");
+            UC1CreateContact uc1 = new UC1CreateContact();
+            uc1.CreateContact();
+
+
 
         }
     }
